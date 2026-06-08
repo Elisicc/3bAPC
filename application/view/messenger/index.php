bf85c7f2c64a7026b9a7fd9bc5ab364ea84ed4a8
Elias Breitler <157363433+Elisicc@users.noreply.github.com>
2026-06-08 19:59:42 +0200

    Added Datatable

--- application/view/messenger/index.php
+++ application/view/messenger/index.php
@@ -1,25 +1,69 @@
-<div class="container">
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
 
+<script>
+$(document).ready(function() {
+    $('#messenger-table').DataTable();
+});
+</script>
+
+<link rel="stylesheet"
+href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css" />
+
+
+<div class="container">
     <h1>Messenger</h1>
 
     <div class="box">
 
-        <h3>Users</h3>
+        <h3>Select a user to chat with</h3>
 
-        <?php foreach ($this->users as $user) { ?>
+        <table class="overview-table">
 
-            <div style="margin-bottom: 10px;">
+            <thead>
+            <tr>
+                <td>Avatar</td>
+                <td>Username</td>
+                <td>New Messages</td>
+                <td>Open Chat</td>
+            </tr>
+            </thead>
 
-                <a href="<?= Config::get('URL'); ?>messenger/chat/<?= $user->user_id; ?>">
+        <table id="messenger-table" class="overview-table">
 
-                    <?= $user->user_name; ?>
-
-                </a>
-
-            </div>
-
-        <?php } ?>
+        <thead>
+    
+        <tr>
+            <td>Avatar</td>
+            <td>Username</td>
+            <td>New Messages</td>
+            <td>Open Chat</td>
+        </tr>
+        </thead>
+            <tbody>
+                <?php foreach ($this->users as $user) { ?>
+                    <tr>
+                        <td class="avatar">
+                            <?php if (isset($user->user_avatar_link)) { ?>
+                                <img src="<?= $user->user_avatar_link; ?>" />
+                            <?php } ?>
+                        </td>
+                        <td>
+                            <?= $user->user_name; ?>
+                        </td>
+                        <td>
+                            0
+                        </td>
+                        <td>
+                            <a href="<?= Config::get('URL'); ?>messenger/chat/<?= $user->user_id; ?>">
+                                Open
+                            </a>
+                        </td>
+                    </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+        </table>
 
     </div>
-
 </div>
