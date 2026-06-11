4c32f4a4650e4ac73bb3467061c15e3a61a9398e
Elias Breitler <157363433+Elisicc@users.noreply.github.com>
2026-06-11 09:21:12 +0200

    Added Button for uploading and publishing

--- application/view/picture/index.php
+++ application/view/picture/index.php
@@ -1,4 +1,22 @@
 <div class="container">
+
+
+<form action="<?php echo Config::get('URL'); ?>picture/upload_action"
+      method="post"
+      enctype="multipart/form-data">
+
+    <input type="file" name="picture_file" required>
+    <input type="submit" value="Upload">
+</form>
+
+<form id="publish-form" action="" method="post">
+    <input id="publish-button" type="submit" value="Publish" disabled>
+</form>
+
+
+</script>
+
+<div class="container">
     <h1>PictureController/index</h1>
     <div class="box">
 
