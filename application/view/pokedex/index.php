1b3474ad20b1a629a2ded663512f2f2f85438f0d
Elias Breitler <157363433+Elisicc@users.noreply.github.com>
2026-06-23 15:46:33 +0200

    Displaying sprites with API

--- application/view/pokedex/index.php
+++ application/view/pokedex/index.php
@@ -2,13 +2,23 @@
     <h1>PokedexController/index</h1>
     <div class="box">
         <h3>What happens here ?</h3>
-        <p>Test</p>
     <ul>
         <?php foreach ($this->pokemon['results'] as $poke) : ?>
-            <li>
-                <?php echo ucfirst($poke['name']); ?>
-            </li>
-        <?php endforeach; ?>
+
+    <?php
+    $pokemonId = basename(rtrim($poke['url'], '/'));
+    $imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" . $pokemonId . ".png";
+    ?>
+
+    <div style="margin-bottom:20px;">
+        <img src="<?= $imageUrl ?>" alt="<?= $poke['name'] ?>">
+        <br>
+        <?= ucfirst($poke['id']) ?>
+        <?= ucfirst($poke['name']) ?>
+        <?= ucfirst($poke['types']) ?>
+    </div>
+
+<?php endforeach; ?>
     </ul>
 </div>
     </div>
