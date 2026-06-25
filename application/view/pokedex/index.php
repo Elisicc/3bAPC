f8b21718b87a52013333c95cf51cdee79cd66b7c
Elias Breitler <157363433+Elisicc@users.noreply.github.com>
2026-06-25 08:48:12 +0200

    Added Data Table for pokemon list

--- application/view/pokedex/index.php
+++ application/view/pokedex/index.php
@@ -1,30 +1,71 @@
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
+
+<script>
+$(document).ready(function () {
+    $('#pokemon-table').DataTable({
+        pageLength: 10,
+        lengthMenu: [10, 20, 50],
+        language: {
+            search: "Pokémon suchen:",
+            lengthMenu: "Zeige _MENU_ Pokémon",
+            info: "Zeige _START_ bis _END_ von _TOTAL_ Pokémon",
+            paginate: {
+                previous: "Zurück",
+                next: "Weiter"
+            }
+        }
+    });
+});
+</script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css" />
 <div class="container">
-    <h1>PokedexController/index</h1>
+    <h1>Pokédex</h1>
     <div class="box">
-        <h3>What happens here ?</h3>
-    <ul>
-        <?php foreach ($this->pokemon['results'] as $poke) : ?>
+        <table id="pokemon-table" class="overview-table">
 
-    <?php
-    $pokemonId = basename(rtrim($poke['url'], '/'));
-    $imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" . $pokemonId . ".png";
-    ?>
+            <thead>
+                <tr>
+                    <th>Bild</th>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Aktion</th>
+                </tr>
+            </thead>
 
-    <div style="margin-bottom:20px;">
-        <img src="<?= $imageUrl ?>" alt="<?= $poke['name'] ?>">
-        <br>
+            <tbody>
 
-        <strong>#<?= $pokemonId; ?></strong>
+            <?php foreach ($this->pokemon['results'] as $poke) : ?>
 
+                <?php
+                $pokemonId = basename(rtrim($poke['url'], '/'));
+                $imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" . $pokemonId . ".png";
+                ?>
+                <tr>
+                    <td class="avatar">
+                        <a href="<?= Config::get('URL') . 'pokedex/details/' . $pokemonId; ?>">
+                            <img src="<?= $imageUrl ?>"
+                                 alt="<?= $poke['name']; ?>"
+                                 width="70">
+                        </a>
+                    </td>
 
-        <a href="<?= Config::get('URL') . 'pokedex/details/' . $pokemonId; ?>">
-            <?= ucfirst($poke['name']) ?>
-        </a>
-
-    </div>
-
-<?php endforeach; ?>
-    </ul>
-</div>
+                    <td>
+                        <?= sprintf("%03d", $pokemonId); ?>
+                    </td>
+                    <td>
+                        <a href="<?= Config::get('URL') . 'pokedex/details/' . $pokemonId; ?>">
+                            <?= ucfirst($poke['name']); ?>
+                        </a>
+                    </td>
+                    <td>
+                        <a href="#">
+                            Zum Team
+                        </a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
     </div>
 </div>
