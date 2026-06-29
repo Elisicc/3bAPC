ca34c39145370312f344ef5d9ffa96d53c639046
Elias Breitler <157363433+Elisicc@users.noreply.github.com>
2026-06-29 09:54:24 +0200

    added improved UI for the team view

--- application/view/pokedex/team.php
+++ application/view/pokedex/team.php
@@ -1,59 +1,98 @@
 <style>
 .team-grid {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 25px;
-    margin-top: 30px;
-}
-.team-slot {
-    height: 180px;
-    border: 2px solid #dcdcdc;
-    border-radius: 10px;
-    background-color: #f8fbfc;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    text-decoration: none;
-
-    transition: all 0.2s ease;
-}
-.team-slot:hover {
-    background-color: #eef7fa;
-    border-color: #6aa9ff;
-    transform: scale(1.03);
+    display:grid;
+    grid-template-columns:repeat(3,1fr);
+    gap:20px;
+    margin-top:30px;
 }
 
-.plus {
-    font-size: 70px;
-    color: #999;
-    font-weight: bold;
+.team-slot{
+    height:180px;
+
+    border:2px solid #ddd;
+    border-radius:10px;
+
+    background:#f8fbfc;
+
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+
+    text-decoration:none;
 }
 
-.team-title {
-    margin-bottom: 10px;
+.team-slot:hover{
+    background:#eef7fa;
+}
+
+.plus{
+    font-size:70px;
+    color:gray;
+    font-weight:bold;
+}
+
+.team-slot img{
+    width:90px;
+}
+
+.team-slot p{
+    margin-top:10px;
+    font-weight:bold;
 }
 </style>
 
-<div class="container team-page">
+<div class="container">
 
-    <h1 class="team-title">Mein Pokémon-Team</h1>
+    <h1>Mein Pokémon-Team</h1>
 
     <div class="box">
 
-        <h3>Wähle einen freien Platz für dein Pokémon</h3>
+        <h3>Wähle einen freien Platz</h3>
 
         <div class="team-grid">
 
-            <?php for ($slot = 1; $slot <= 6; $slot++) : ?>
+        <?php
+
+        $teamSlots = [];
+
+        foreach($this->team as $member){
+            $teamSlots[$member['slot']] = $member['pokemon_id'];
+        }
+
+        ?>
+
+        <?php for($slot = 1; $slot <= 6; $slot++) : ?>
+
+            <?php if(isset($teamSlots[$slot])) : ?>
+
+                <div class="team-slot">
+
+                    <?php
+                    $sprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/".$teamSlots[$slot].".png";
+                    ?>
+
+                    <img src="<?= $sprite ?>">
+
+                    <p>#<?= sprintf("%03d",$teamSlots[$slot]); ?></p>
+
+                </div>
+
+            <?php else : ?>
 
                 <a class="team-slot"
-                   href="<?= Config::get('URL') . 'pokedex/saveTeam/' . $this->pokemonId . '/' . $slot; ?>">
+                   href="<?= Config::get('URL').'pokedex/saveTeam/'.$this->pokemonId.'/'.$slot; ?>">
 
                     <div class="plus">+</div>
+
                 </a>
-            <?php endfor; ?>
+
+            <?php endif; ?>
+
+        <?php endfor; ?>
+
         </div>
+
     </div>
+
 </div>
